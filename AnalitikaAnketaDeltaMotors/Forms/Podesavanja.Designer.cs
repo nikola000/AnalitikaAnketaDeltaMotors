572e9c1e7985f808174a55e38b30573356b3674b
Marko <mrakic86@gmail.com>
2021-07-14 12:31:13 +0200

    Podesavanja layout

--- AnalitikaAnketaDeltaMotors/Forms/Podesavanja.Designer.cs
+++ AnalitikaAnketaDeltaMotors/Forms/Podesavanja.Designer.cs
@@ -41,13 +41,13 @@ namespace AnalitikaAnketaDeltaMotors.Forms
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.Location = new System.Drawing.Point(12, 29);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(738, 20);
+            this.textBox1.Size = new System.Drawing.Size(409, 20);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.button1.Location = new System.Drawing.Point(594, 28);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(495, 27);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(180, 23);
             this.button1.TabIndex = 1;
@@ -61,9 +61,10 @@ namespace AnalitikaAnketaDeltaMotors.Forms
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(799, 26);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(427, 27);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(137, 23);
+            this.button2.Size = new System.Drawing.Size(62, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Test";
             this.button2.UseVisualStyleBackColor = true;
@@ -73,7 +74,7 @@ namespace AnalitikaAnketaDeltaMotors.Forms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(948, 390);
+            this.ClientSize = new System.Drawing.Size(687, 279);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
