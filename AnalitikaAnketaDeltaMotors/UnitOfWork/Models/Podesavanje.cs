ac28078f26e2b0c1b693e15751ddd2045b2b68e9
Andrija Rajovic <andrijaogi@gmail.com>
2021-07-16 14:22:18 +0200

    Model podesavanja

---
+++ AnalitikaAnketaDeltaMotors/UnitOfWork/Models/Podesavanje.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalitikaAnketaDeltaMotors.UnitOfWork.Models
+{
+    class Podesavanje
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+}
